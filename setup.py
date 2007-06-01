e65f683803221fdf6becbf8a43597cc05dd200c5
Daniel Holbach <daniel.holbach@ubuntu.com>
2007-06-01 11:44:28 +0200

    initial packaging - please let me know if you're not OK with GPL and add yourself to AUTHORS

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import glob
+import os
+import re
+
+# look/set what version we have
+changelog = "debian/changelog"
+if os.path.exists(changelog):
+    head=open(changelog).readline()
+    match = re.compile(".*\((.*)\).*").match(head)
+    if match:
+        version = match.group(1)
+   
+setup(name='ubuntu-dev-tools',
+      version=version,
+      scripts=['404main', 'check-symbols', 'pbuilder-dist', 'ubuntu-cd',
+      'ubuntu-sync', 'update-maintainer'],
+)
+
