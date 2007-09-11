52a506f398ec94397ed59d856db2684748203231
Steve Kowalik <stevenk@ubuntu.com>
2007-09-12 01:23:54 +1000

    Add requestsync to setup.py, and myself to AUTHORS.

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(name='ubuntu-dev-tools',
                'pull-debian-debdiff',
                'what-patch',
                'suspicious-source',
-               'revuput'
+               'revuput',
+               'requestsync'
   	       ],
 )
 
