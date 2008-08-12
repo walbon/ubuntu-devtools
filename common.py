0faf0bc3e59e2fe3bd425e7ef762caf7df616b5d
Siegfried-Angel Gevatter Pujals <rainct@ubuntu.com>
2008-08-12 20:15:15 +0200

    Relicense Kees' scripts to GPLv3+. Add sbuild as alternative recommend to pbuilder.

--- common.py
+++ common.py
@@ -1,27 +1,28 @@
 #
-#   common.py - provides functions which are commonly used by the
-#               ubuntu-dev-tools package.
+# common.py - provides functions which are commonly used by the
+#             ubuntu-dev-tools package.
 #
-#   Copyright (C) 2008 Jonathan Patrick Davies <jpds@ubuntu.com>
+# Copyright (C) 2008 Jonathan Patrick Davies <jpds@ubuntu.com>
 #
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+# Some of the functions are based upon code written by Martin Pitt
+# <martin.pitt@ubuntu.com> and Kees Cook <kees@ubuntu.com>.
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+# ##################################################################
 #
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
+# See file /usr/share/common-licenses/GPL for more details.
+#
+# ##################################################################
 
-# Based on code written by Martin Pitt <martin.pitt@ubuntu.com> and
-# Kees Cook <kees@ubuntu.com>.
-
-""" Necessary modules. """
 import cookielib
 import glob
 import os.path
