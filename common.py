3c25fca14276325a4df4821ec3807692e18c3f49
Jonathan Patrick Davies <jpds@ubuntu.com>
2008-08-28 19:21:45 +0100

    * common.py: New function: isLPTeamMember() - checks if the user is a member of   the Launchpad team using cookies for authentication. * requestsync: Adapt team checking with the function above. * buildd: Adapt privilege checking code to the new function above.

--- common.py
+++ common.py
@@ -202,3 +202,34 @@ def setupLaunchpadUrlOpener(cookie):
     urlopener.add_handler(urllib2.HTTPCookieProcessor(cj))
     
     return urlopener
+
+def isLPTeamMember(team):
+    """ Checks if the user is a member of a certain team on Launchpad.
+
+        We do this by opening the team page on Launchpad and checking if the
+        text "You are not a member of this team" is present using the
+        user's cookie file for authentication.
+
+        If the user is a member of the team: return True.
+        If the user is not a member of the team: return False.
+    """
+
+    # TODO: Check if launchpadlib may be a better way of doing this.
+
+    # Prepare cookie.
+    cookieFile = prepareLaunchpadCookie()
+    # Prepare URL opener.
+    urlopener = setupLaunchpadUrlOpener(cookieFile)
+
+    # Try to open the Launchpad team page:
+    try:
+        lpTeamPage = urlopener.open("https://launchpad.net/~%s" % team).read()
+    except urllib2.HTTPError, error:
+        print >> sys.stderr, "Unable to connect to Launchpad. Received a %s." % error.code
+        sys.exit(1)
+
+    # Check if text is present in page.
+    if ("You are not a member of this team") in lpTeamPage:
+        return False
+
+    return True
