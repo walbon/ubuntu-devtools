324c2957fd10e30742550fa0e466bd4bf24d2a03
Stefano Rivera <stefanor@ubuntu.com>
2010-12-20 20:56:38 +0200

    ubuntutools/common.py: Remove https_proxy unsetting code, working around LP: #94130.

--- ubuntutools/common.py
+++ ubuntutools/common.py
@@ -15,16 +15,6 @@
 # OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-import os
-import sys
-
-# Clear https_proxy env var as it's not supported in urllib/urllib2; see
-# LP #122551
-if os.environ.has_key('https_proxy'):
-    print >> sys.stderr, "Ignoring https_proxy (no support in urllib/urllib2; see LP #122551)"
-    del os.environ['https_proxy']
-
-
 def memoize_noargs(func):
     "Simple memoization wrapper, for functions without arguments"
     func.cache = None
