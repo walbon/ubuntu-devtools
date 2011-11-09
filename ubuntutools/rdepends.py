fed4e1718879105d0cc6bdf51e07b3e7957278c5
Stefano Rivera <stefanor@ubuntu.com>
2011-11-09 23:44:22 +0200

    Rdepends client tool

---
+++ ubuntutools/rdepends.py
@@ -0,0 +1,45 @@
+# Copyright (C) 2011, Stefano Rivera <stefanor@debian.org>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import json
+import os
+import urllib2
+
+
+def rdepends(package, release, arch, recommends=True, suggests=False,
+             server='http://qa.ubuntuwire.org/rdepends'):
+    """Look up a packages reverse-dependencies on the Ubuntuwire
+    Reverse- webservice
+    """
+    url = os.path.join(server, 'v1', release, arch, package)
+    try:
+        data = json.load(urllib2.urlopen(url))
+    except urllib2.HTTPError, e:
+        if e.code == 404:
+            return []
+        raise
+
+    if arch == 'source':
+        fields = ('Build-Depends', 'Build-Depends-Indep')
+    else:
+        fields = ['Depends']
+        if recommends:
+            fields.append('Recommends')
+        if suggests:
+            fields.append('Suggests')
+
+    result = set()
+    for field in fields:
+        result.update(data.get(field, []))
+    return list(result)
