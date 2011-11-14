82951a2716e69064d45fa695b50c1dbd41006fdf
Stefano Rivera <stefanor@ubuntu.com>
2011-11-15 01:50:55 +0200

    Copyright notice

--- ubuntutools/lp/lpapicache.py
+++ ubuntutools/lp/lpapicache.py
@@ -4,6 +4,7 @@
 #                   for usage in the ubuntu-dev-tools package
 #
 #   Copyright © 2009-2010 Michael Bienia <geser@ubuntu.com>
+#               2011      Stefano Rivera <stefanor@ubuntu.com>
 #
 #   This program is free software; you can redistribute it and/or
 #   modify it under the terms of the GNU General Public License
