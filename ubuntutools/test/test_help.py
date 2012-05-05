b26dbaee681e400a52706c001b486900e8fdc4c4
Stefano Rivera <stefanor@ubuntu.com>
2012-05-05 19:37:41 +0200

    check-mir: Use optparse to display --help

--- ubuntutools/test/test_help.py
+++ ubuntutools/test/test_help.py
@@ -25,7 +25,6 @@ from ubuntutools import subprocess
 from ubuntutools.test import unittest
 
 BLACKLIST = {
-    'check-mir': 'No Help',
     'check-symbols': 'No Help',
     'grep-merges': 'No Help',
     'pbuilder-dist-simple': 'No Help',
